18c70e423b7941c51be408b2ebae85e87f67551d
SariSichel <sarisichel4@gmail.com>
2025-11-20 17:50:59 +0200

    fix max and min to size in validation

--- backend/project/src/main/java/com/example/project/dto/UserSignInDTO.java
+++ backend/project/src/main/java/com/example/project/dto/UserSignInDTO.java
@@ -3,15 +3,17 @@ package com.example.project.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class UserSignInDTO {
 
     @NotNull
-    @Min(value = 2, message = "name must be at least 2 chars")
-    @Max(value = 50, message = "name must be at most 50 chars")
+    @Size(min = 2, max=50 , message = "name must be at least 2 chars and at most 50 chars")
+
     private String Name;
     @NotNull
-    @Min(value = 4, message = "password must be at least 4 chars")
+    @Size(min = 4, message = "password must be at least 4 chars")
+
     private String password;
 
     public String getName() {
